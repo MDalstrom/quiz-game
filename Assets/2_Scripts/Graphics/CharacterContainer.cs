b4012d735fb0f1d19736920cf234effabd588617
mdalstrom <mdalstrom@yandex.ru>
2022-04-15 00:38:35 +0700

    character container script

---
+++ Assets/2_Scripts/Graphics/CharacterContainer.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+namespace QuizGame.Graphics
+{
+    public class CharacterContainer : MonoBehaviour
+    {
+        [SerializeField] private CharacterButton buttonPrefab;
+        [SerializeField] private Transform root;
+
+        private readonly List<CharacterButton> buttons = new List<CharacterButton>();
+
+        public void UpdateView(char?[] word)
+        {
+            ResetWord();
+
+            for (int i = 0; i < word.Length; i++)
+            {
+                AddCharacter(word[i]);
+            }
+        }
+        public void UpdateView(string word)
+        {
+            UpdateView(word);
+        }
+
+        /// <summary>
+        /// Fill up container with empty word of given length
+        /// </summary>
+        public void UpdateView(int length)
+        {
+            UpdateView(new char?[length]);
+        }
+
+        private void ResetWord()
+        {
+            buttons.ForEach(x => Destroy(x));
+            buttons.Clear();
+        }
+
+        private CharacterButton AddCharacter(char? value)
+        {
+            var button = Instantiate(buttonPrefab, root);
+            button.Init();
+            button.UpdateView(value);
+            return button;
+        }
+    }
+}
