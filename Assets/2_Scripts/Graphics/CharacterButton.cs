172c81f95faffa4b2d68e345792312708404cd97
mdalstrom <mdalstrom@yandex.ru>
2022-04-15 00:30:47 +0700

    character button scriptp

---
+++ Assets/2_Scripts/Graphics/CharacterButton.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+namespace QuizGame.Graphics
+{
+    public class CharacterButton : MonoBehaviour
+    {
+        [SerializeField] private Button button;
+        [Header("Enabled state")]
+        [SerializeField] private GameObject enabledStateObject;
+        [SerializeField] private Text valueText;
+        [Header("Disabled state")]
+        [SerializeField] private GameObject disabledStateObject;
+
+        public event UnityAction onClick;
+
+        public void Init()
+        {
+            button.onClick.AddListener(onClick);
+        }
+
+        public void UpdateView(char? value)
+        {
+            enabledStateObject.SetActive(value.HasValue);
+            disabledStateObject.SetActive(!value.HasValue);
+
+            valueText.text = value?.ToString().ToUpper();
+        }
+    }
+}
